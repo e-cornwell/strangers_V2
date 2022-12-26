82a37e521f1310e8076c67340621d1b146bfe539
Ethan Cornwell <ethan.aubrey22@gmail.com>
2022-12-26 13:26:51 -0700

    functioning login and registration

--- src/index.js
+++ src/index.js
@@ -4,12 +4,94 @@ import { HashRouter, Routes, Route, Link} from 'react-router-dom';
 
 
 const App = ()=> {
-  const [posts, setPosts] = useState([]);
-  
+  //const [posts, setPosts] = useState([]);
+  const [registerUsername, setRegisterUsername] = useState('');
+  const [registerPassword, setRegisterPassword] = useState('');
+  const [loginUsername, setLoginUsername] = useState('');
+  const [loginPassword, setLoginPassword] = useState('');
+  const [user, setUser] = useState({});
 
-  
+  const register = (ev)=> {
+    ev.preventDefault();
+    console.log('hello world')
+    fetch('https://strangers-things.herokuapp.com/api/2209-FTB-ET-WEB-AM/users/register', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        user: {
+          username: registerUsername,
+          password: registerPassword
+        }
+      })
+    })
+    .then(response => response.json())
+    .then(result => {
+      console.log(result);
+    })
+    .catch( err => console.log(err));
+  }
+
+  const login = (ev)=> {
+    ev.preventDefault();
+    console.log('login');
+    fetch('https://strangers-things.herokuapp.com/api/2209-FTB-ET-WEB-AM/users/login', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        user: {
+          username: loginUsername,
+          password: loginPassword
+        }
+      })
+    })
+    .then(response => response.json())
+    .then(result => {
+      const token = result.data.token;
+      window.localStorage.setItem('token', token);
+      console.log(token);
+      fetch('https://strangers-things.herokuapp.com/api/2209-FTB-ET-WEB-AM/users/me', {
+        headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      },
+      })
+      .then(response => response.json())
+      .then(result => {
+        const user = result.data;
+        setUser(user);
+      })
+      .catch(console.error);
+    })
+    .catch( err => console.log(err));
+  }
+
   useEffect(()=> {
-  }, [])
+    const token = window.localStorage.getItem('token');
+    if(token){
+      fetch('https://strangers-things.herokuapp.com/api/2209-FTB-ET-WEB-AM/users/me', {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${ token }`
+        },
+      })
+      .then(response => response.json())
+      .then(result => {
+        const user = result.data;
+        setUser(user);
+      })
+      .catch(err => console.log(err));
+    }
+  }, []);
+
+  const logout = () => {
+    window.localStorage.removeItem('token');
+    setUser({});
+  }
+  
   return (
     <div>
       <h1>Strangers Things V2</h1>
@@ -23,16 +105,37 @@ const App = ()=> {
         <Route path='/login' element={ <div>Login</div>} />} />
         <Route path='/register' element={ <div>Register</div>} />} />
       </Routes>  */}
-      <form>
-        <input placeholder='username' />
-        <input placeholder='password' />
-        <button>Register</button>
-      </form>
-      <form>
-        <input placeholder='username' />
-        <input placeholder='password' />
-        <button>Login</button>
-      </form>
+      {
+        user._id ? <div>Welcome { user.username } <button onClick={ logout }>Logout</button> </div> : null
+      }
+      {
+        !user._id ? (
+        <div>
+          <form onSubmit = { register } >
+            <input 
+              placeholder='username' 
+              value={ registerUsername } 
+              onChange={ ev => setRegisterUsername(ev.target.value) } />
+            <input 
+              placeholder='password'
+              value={ registerPassword } 
+              onChange={ ev => setRegisterPassword(ev.target.value) } />
+            <button>Register</button>
+          </form>
+
+          <form onSubmit = { login } >
+            <input 
+              placeholder='username'
+              value={ loginUsername} 
+              onChange={ ev => setLoginUsername(ev.target.value)} />
+            <input 
+              placeholder='password'
+              value={ loginPassword} 
+              onChange={ ev => setLoginPassword(ev.target.value)} />
+            <button>Login</button>
+          </form>
+        </div>) : null
+      }
     </div>
 
   );
